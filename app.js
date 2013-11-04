749aea5e2072297e92a1e41551eba223a23e8bdb
Silver Kuusik <silver.kuusik@gmail.com>
2013-11-04 18:27:50 +0100

    added user counter

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var redis = require('redis');
 var app = express();
+var db = redis.createClient();
 
+/* user count */
+app.use(function(req, res, next){
+  var min = 60 * 1000;
+  var ago = Date.now() - min;
+  db.zrevrangebyscore('online', '+inf', ago, function(err, users){
+    if (err) return next(err);
+    req.online = users;
+    next();
+  });
+});
 /* routes */
-app.get('/hello.txt', function(req, res){
-  res.send('Sum Ting Work');
+app.get('/', function(req, res){
+  res.send('Sum Ting Work, ' + req.online.length + ' users are online');
 });
 /* error handling */
 app.use(function(err, req, res, next){
