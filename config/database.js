3642e87116b7e96d7e22621c63ba7bc391b21119
Silver Kuusik <silver.kuusik@gmail.com>
2013-11-26 02:11:55 +0100

    added generation of password for google and facebook

--- config/database.js
+++ config/database.js
@@ -1,30 +1,88 @@
-/* Configuration */
-var config = require("./config");
+/* Add modules */
+var config = require("./config"),
+	auth = require("./authentication"),
+	email = require("./email");
 
 /* Connect to database */
-console.log("INFO", "connecting to database on:", config.database_url);
 var mongoose = require("mongoose");
 var collections = ["users", "reports"];
+console.log("INFO", "connecting to database on:", config.database_url);
 mongoose.connect(config.database_url, collections);
+/* Debug mode */
+mongoose.set('debug', true);
 
 var Schema = mongoose.Schema,
-  ObjectId = Schema.ObjectId;
+	ObjectId = Schema.ObjectId;
 
 /* Database schemas */
-var UserSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-  password: { type: String, required: true },
-  verified: { type: Boolean, required: true },
-  verification_hash: { type: String, required: true, unique: true },
-  providers: [{ name: String, mugshot_src: String, profile_name: String, profile_url: String }],
-  badges: [{ name: String, date: Date }]
-});
 var BadgeSchema = new Schema({
-  name: { type: String, required: true, unique: true },
-  image: { type: String, required: true },
+	name: { type: String, unique: true, required: true },
+	image: { type: String, required: true }
+});
+
+var ProviderSchema = new Schema({
+	name: { type: String, required: true },
+	mugshot: String,
+	display_name: String,
+	url: String
+});
+
+var CourseSchema = new Schema({
+	name: { type: String, unique: true, required: true },
+	description: String,
+	difficulty: String,
+	keywords: [{ type: String }],
+	levels: [{ task: String, points: Number, badge: { type: ObjectId, ref: 'Badge' } }]
+});
+
+var UserSchema = new Schema({
+	name: { type: String, required: true },
+	email: { type: String, unique: true, lowercase: true, trim: true, required: true, validate: /\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/ },
+	password: { type: String, required: true, default: "default" },
+	verification: {
+		verified: { type: Boolean, required: true, default: false }, 
+		verification_hash: { type: String, required: true, unique: true, default: "default" }
+	},
+	profile: {
+		joined_date: { type: Date, required: true, default: Date.now },
+		points: { type: Number, default: 1 },
+		location: { type: String, default: "" },
+		mugshot: { type: String, default: "" },
+		website: { type: String, default: "" },
+		description: { type: String, default: "" },
+		providers: [{ type: ObjectId, ref: 'Provider' }],
+		badges: [{ type: ObjectId, ref: 'Badge' }],
+	},
+	courses: [{ progress: Number, course: { type: ObjectId, ref: 'Course' } }]
+});
+
+/* Predave for user */
+UserSchema.pre('save', function(next) {
+	if (this.password == "default") {
+		/* Send the user his default password */
+		this.password = auth.calculateHash("md5", this.email + Date.now);
+		email.sendLoginEmail(this.name, this.email, this.password);
+		/* Calculate the password hash */
+		this.password = auth.calculateHash("sha1", this.password);
+		/* Set user as verified */
+		this.verification.verified = true;
+		this.verification.verification_hash = auth.calculateHash("sha1", this.email + Date.now);
+		return next();
+	}
+	/* When the verification_hash has default value */
+	if (this.verification.verification_hash == "default") {
+		/* Calculate the password hash */
+		this.password = auth.calculateHash("sha1", this.password);
+		/* Calculate the verification hash */
+	  this.verification.verification_hash = auth.calculateHash("sha1", this.email + Date.now);
+	  /* Send the user the verification email */
+	  email.sendRegistrationEmail(this.name, this.email, this.verification.verification_hash);
+	}
+  next();
 });
 
 /* Database objects */
-exports.User = mongoose.model("User", UserSchema);
 exports.Badge = mongoose.model("Badge", BadgeSchema);
+exports.Provider = mongoose.model("Provider", ProviderSchema);
+exports.Course = mongoose.model("Course", CourseSchema);
+exports.User = mongoose.model("User", UserSchema);
