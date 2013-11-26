95de5563bb93d5d86de53e72ad633ef820fbb192
Silver Kuusik <silver.kuusik@gmail.com>
2013-11-26 02:11:16 +0100

    added emailing password for google and facebook login

--- config/email.js
+++ config/email.js
@@ -11,12 +11,24 @@ var server = email.server.connect({
 
 exports.sendRegistrationEmail = function sendEmail(name, email, hash) {
 	server.send({
-	   text:    "Welcome to CodeBuddy, verify your registration under this link " + config.hostname + "/register/" + hash, 
-	   from:    "CodeBuddyWeb <codebuddyweb@gmail.com>", 
-	   to:      name + " <" + email + ">",
+	   text: "Welcome to CodeBuddy, verify your registration under this link " + config.hostname + "/register/" + hash,
+	   from: "CodeBuddyWeb <codebuddyweb@gmail.com>",
+	   to: name + " <" + email + ">",
 	   subject: "CodeBuddy registration"
-	}, function(err, message) { 
+	}, function(err, message) {
 		if (err) console.log("ERROR", "sending email:", err);
-		else console.log("INFO", "successfully sent email:", message); 
+		else console.log("INFO", "successfully sent email:", message);
+	});
+}
+
+exports.sendLoginEmail = function sendEmail(name, email, password) {
+	server.send({
+	   text: "Welcome to CodeBuddy, your default password is: " + password + " login at " + config.hostname + "/login",
+	   from: "CodeBuddyWeb <codebuddyweb@gmail.com>",
+	   to: name + " <" + email + ">",
+	   subject: "CodeBuddy registration"
+	}, function(err, message) {
+		if (err) console.log("ERROR", "sending email:", err);
+		else console.log("INFO", "successfully sent email:", message);
 	});
 }
