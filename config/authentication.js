cf314d2d2db44dad5d84b2a0f116b0f22573d268
Silver Kuusik <silver.kuusik@gmail.com>
2013-11-26 02:15:34 +0100

    added provider info for the user when google or facebook login

--- config/authentication.js
+++ config/authentication.js
@@ -35,7 +35,7 @@ passport.use(new LocalStrategy(
   function(username, password, done) {
     process.nextTick(function () {
       /* So the user can also write the first part of the email, example@email.com can be also example */
-      db.User.findOne({ email: new RegExp("(^" + username + "@|^" + username + "$)", "i") }, function (err, user) {
+      db.User.findOne({ email: new RegExp("(^" + username + "@|^" + username + "$)", "i") }).populate('profile.providers').exec(function (err, user) {
         if (err) return done(err);
         if (!user) return done(null, false);
         if (user.password != exports.calculateHash("sha1", password)) return done(null, false);
@@ -52,13 +52,31 @@ passport.use(new GoogleStrategy({
   },
   function(accessToken, refreshToken, profile, done) {
   	process.nextTick(function () {
-      console.log("INFO", profile);
-      var user = {};
-      user.mugshot = profile._json.picture || profile._json.profile_image_url;
-      user.name = profile.displayName || "Anonymous";
-      user.link = profile._json.link || profile._json.url || profile.profileUrl;
-      user.email = profile._json.email || "";
-	    return done(null, user);
+      //console.log("INFO", "google user info:", accessToken);
+      db.User.findOne({ email: profile._json.email }).populate('profile.providers').exec(function (err, user) {
+        if (err) return done(err);
+        /* When the user was found and the provider is registered */
+        else if (user && user.profile.providers.map(function(elem) { return elem.name; }).join(",").indexOf(profile.provider) > -1) return done(null, user);
+
+        var provider = new db.Provider({ name: profile.provider, mugshot: profile._json.picture, display_name: profile._json.name, url: profile._json.link });
+        provider.save(function(err) {
+          if (err) console.log("ERROR", "error saving provider:", err);
+          else console.log("INFO", "provider saved:", provider.name);
+        });
+        var user = new db.User({ name: profile._json.name, email: profile._json.email });
+        user.profile.mugshot = profile._json.picture;
+        user.profile.website = profile._json.link;
+        user.profile.providers.push(provider);
+        user.save(function(err) {
+          if (err) console.log("ERROR", "error saving user:", err);
+          else {
+            console.log("INFO", "user saved:", user.email);
+            db.User.findOne({ email: profile._json.email }).populate('profile.providers').exec(function (err, user) {
+              return done(null, user);
+            });
+          }
+        });
+      });
 	  });
   }
 ));
@@ -72,10 +90,10 @@ passport.use(new TwitterStrategy({
     process.nextTick(function () {
       console.log("INFO", profile);
       var user = {};
-      user.mugshot = profile._json.picture || profile._json.profile_image_url;
-      user.name = profile.displayName || "Anonymous";
-      user.link = profile._json.link || profile._json.url || profile.profileUrl;
-      user.email = profile._json.email || "";
+      user.mugshot = profile._json.profile_image_url;
+      user.name = profile.displayName;
+      user.website = profile._json.url;
+      user.profile = profile;
 	    return done(null, user);
 	  });
   }
@@ -84,17 +102,34 @@ passport.use(new TwitterStrategy({
 passport.use(new FacebookStrategy({
     clientID: config.facebook.consumer_key,
     clientSecret: config.facebook.consumer_secret,
-    callbackURL: config.facebook.callback
+    callbackURL: config.facebook.callback,
+    profileFields: ['emails', 'displayName', 'photos', 'profileUrl']
   },
   function(accessToken, refreshToken, profile, done) {
-  	process.nextTick(function () {
-      console.log("INFO", profile);
-      var user = {};
-      user.mugshot = profile._json.picture || profile._json.profile_image_url;
-      user.name = profile.displayName || "Anonymous";
-      user.link = profile._json.link || profile._json.url || profile.profileUrl;
-      user.email = profile._json.email || "";
-    	return done(null, user);
-  	});
+    process.nextTick(function () {
+      //console.log("INFO", "facebook user info:", profile);
+      db.User.findOne({ email: profile._json.email }).populate('profile.providers').exec(function (err, user) {
+        if (err) return done(null, false);
+        /* When the user was found and the provider is registered */
+        else if (user && user.profile.providers.map(function(elem) { return elem.name; }).join(",").indexOf(profile.provider) > -1) return done(null, user);
+
+        var provider = new db.Provider({ name: profile.provider, mugshot: profile._json.picture, display_name: profile._json.name, url: profile._json.link });
+        provider.save(function(err) {
+          if (err) console.log("ERROR", "error saving provider:", err);
+          else console.log("INFO", "provider saved:", provider.name);
+        });
+        var user = new db.User({ name: profile._json.name, email: profile._json.email });
+        user.profile.mugshot = profile._json.picture.data.url;
+        user.profile.website = profile._json.link;
+        user.profile.providers.push(provider);
+        user.save(function(err) {
+          if (err) console.log("ERROR", "error saving user:", err);
+          else {
+            console.log("INFO", "user saved:", user.email);
+            return done(null, user);
+          }
+        });
+      });
+    });
   }
 ));
