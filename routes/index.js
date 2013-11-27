e568e3b43b53c19199761f5206962d2431f86091
Silver Kuusik <silver.kuusik@gmail.com>
2013-11-27 18:15:30 +0100

    added showing all users

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 /* Add modules */
-var config = require("../config/config");
+var config = require("../config/config"),
+	db = require("../config/database");
 
 /* Homepage */
 exports.index = function(req, res) {
-  res.render('index', { user: req.user });
+	db.User.find(function (err, users) {
+		if (err) console.log("ERROR", "fetching all users:", err);
+		res.render('index', { user: req.user, users: users });
+	});
 };
