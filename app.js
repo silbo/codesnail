80c150ea9f33ca661be0ade0dda7c90306017579
silps <silver.kuusik@gmail.com>
2014-01-03 01:01:17 +0100

    added ace

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ var express = require('express'),
 	passport = require('passport'),
 	auth = require("./config/authentication"),
 	db = require("./config/database"),
-	//MongoStore = require('connect-mongo')(express),
+	MongoStore = require('connect-mongo')(express),
 	config = require("./config/config"),
 	email = require("./config/email"),
 	routes = require('./routes'),
 	user = require('./routes/user');
 
-//var SessionStore = new MongoStore({ url: config.database_url });
+var SessionStore = new MongoStore({ url: config.database_url });
 /* Set app properties */
 app.set('title', "CodeBuddy");
 app.set('view engine', 'jade');
@@ -28,9 +28,9 @@ app.use(expressValidator());
 app.use(express.logger('dev'));
 app.use(express.methodOverride());
 app.use(express.cookieParser());
-app.use(express.session({ 
+app.use(express.session({
 	secret: "super-secret-u-will-never-guess",
-	//store: SessionStore
+	store: SessionStore
 }));
 app.use(flash());
 app.use(passport.initialize());
@@ -81,74 +81,90 @@ app.get("/profile/remove/:name", auth.ensureAuthenticated, user.providerRemove);
 /* Delete all the users and providers */
 if (false) {
 	db.User.remove(function (err, removed) {
-	  if (err) console.log("ERROR", "deleting all users:", err);
-	  else console.log("INFO", "successfully removed all users");
+		if (err) console.log("ERROR", "deleting all users:", err);
+		else console.log("INFO", "successfully removed all users");
 	});
 	db.Provider.remove(function (err, removed) {
-	  if (err) console.log("ERROR", "deleting all providers:", err);
-	  else console.log("INFO", "successfully removed all providers");
+		if (err) console.log("ERROR", "deleting all providers:", err);
+		else console.log("INFO", "successfully removed all providers");
 	});
 }
 
 /* Show all the users and providers */
 db.User.find(function (err, users) {
-  if (err) console.log("ERROR", "fetching all users:", err);
-  else users.forEach(function(user) {
-  	console.log("INFO", "user info:", user);
-  });
+	if (err) console.log("ERROR", "fetching all users:", err);
+	else users.forEach(function(user) {
+		console.log("INFO", "user info:", user);
+	});
 });
 db.Provider.find(function (err, providers) {
-  if (err) console.log("ERROR", "fetching all providers:", err);
-  else providers.forEach(function(provider) {
-  	console.log("INFO", "provider info:", provider);
-  });
+	if (err) console.log("ERROR", "fetching all providers:", err);
+	else providers.forEach(function(provider) {
+		console.log("INFO", "provider info:", provider);
+	});
 });
 
 /* Start the app */
-var server = http.createServer(app).listen(config.port, config.host, function() {
-  console.log("INFO", "express server listening on port:", config.port);
+var server = http.createServer(app).listen(config.port, function() {
+	console.log("INFO", "express server listening on port:", config.port);
 });
 
 /* Setup socket sessionstore */
 var io = require('socket.io').listen(server);
-/*
+
 var passportSocketIo = require('passport.socketio');
 io.set('authorization', passportSocketIo.authorize({
-  cookieParser: express.cookieParser,
-  key: 'connect.sid',
-  secret: 'super-secret-u-will-never-guess',
-  store: SessionStore,
-  fail: function(data, accept) {
-  	console.log("ERROR", "scoket data:", data);
-  	accept(null, false);
-  },
-  success: function(data, accept) {
-  	//console.log("INFO", "scoket:", data);
-    accept(null, true);
-  }
+	cookieParser: express.cookieParser,
+	key: 'connect.sid',
+	secret: 'super-secret-u-will-never-guess',
+	store: SessionStore,
+	fail: function(data, accept) {
+		console.log("ERROR", "scoket data:", data);
+		accept(null, false);
+	},
+	success: function(data, accept) {
+		//console.log("INFO", "scoket:", data);
+		accept(null, true);
+	}
 }));
-*/
-//var onlineUsers = {};
+
+/* Online users */
+var onlineUsers = {};
+/* User initiated socket connection */
 io.sockets.on('connection', function (socket) {
 	/* Add user to online users */
 	console.log("INFO", "socket connection established");
-	/*console.log("INFO", "socket user:", socket.handshake.user.email);
+	console.log("INFO", "socket user:", socket.handshake.user.email);
 	onlineUsers[socket.handshake.user.email] = {
 		name: socket.handshake.user.name,
+		email: socket.handshake.user.email,
 		profile: {
 			mugshot: socket.handshake.user.profile.mugshot,
 			website: socket.handshake.user.profile.website,
 			description: socket.handshake.user.profile.description
 		}
-	};*/
+	};
 	/* Update the online users for all users */
-	io.sockets.emit("users", {});
+	io.sockets.emit("users", onlineUsers);
 
-  /* Delete user from online users */
-  socket.on('disconnect', function() {
-  	//console.log("INFO", "socket user disconnected:", socket.handshake.user.email);
-  	//delete onlineUsers[socket.handshake.user.email];
-  	/* Update the online users for all users */
-  	io.sockets.emit("users", {});
+	/* User sends his/her code */
+	socket.on('get-code', function(userEmail) {
+		console.log("INFO", "get user code:", userEmail);
+		io.sockets.emit("receive-code", onlineUsers[userEmail].code);
+	});
+
+	/* User sends his/her code */
+	socket.on('set-code', function(code) {
+		console.log("INFO", "set user code:", code);
+		onlineUsers[socket.handshake.user.email].code = code;
+	});
+
+	/* User disconnected from socket */
+	socket.on('disconnect', function() {
+		console.log("INFO", "socket user disconnected:", socket.handshake.user.email);
+		/* Delete user from online users */
+		delete onlineUsers[socket.handshake.user.email];
+		/* Update the online users for all users */
+		io.sockets.emit("users", onlineUsers);
 	});
 });
