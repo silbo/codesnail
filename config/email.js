6f92eaa4740b6c9f20019125d915da82a05e55b5
silps <silver.kuusik@gmail.com>
2014-01-05 10:49:39 +0100

    refactor style

--- config/email.js
+++ config/email.js
@@ -3,18 +3,18 @@ var config = require("./config");
 
 var email = require("../node_modules/emailjs/email.js");
 var server = email.server.connect({
-  user: config.smtp.username,
-  password: config.smtp.password,
-  host: "smtp.gmail.com",
-  ssl: true
+	user: config.smtp.username,
+	password: config.smtp.password,
+	host: "smtp.gmail.com",
+	ssl: true
 });
 
 exports.sendRegistrationEmail = function sendEmail(name, email, hash) {
 	server.send({
-	   text: "Welcome to CodeBuddy, verify your registration under this link " + config.hostname + "/register/" + hash,
-	   from: "CodeBuddyWeb <codebuddyweb@gmail.com>",
-	   to: name + " <" + email + ">",
-	   subject: "CodeBuddy registration"
+		text: "Welcome to CodeBuddy, verify your registration under this link " + config.hostname + "/register/" + hash,
+		from: "CodeBuddyWeb <codebuddyweb@gmail.com>",
+		to: name + " <" + email + ">",
+		subject: "CodeBuddy registration"
 	}, function(err, message) {
 		if (err) console.log("ERROR", "sending email:", err);
 		else console.log("INFO", "successfully sent email:", message);
@@ -23,10 +23,10 @@ exports.sendRegistrationEmail = function sendEmail(name, email, hash) {
 
 exports.sendForgotPassword = function sendEmail(name, email, password) {
 	server.send({
-	   text: "CodeBuddy, your new password is: " + password + " login at " + config.hostname + "/login",
-	   from: "CodeBuddyWeb <codebuddyweb@gmail.com>",
-	   to: name + " <" + email + ">",
-	   subject: "CodeBuddy password"
+		text: "CodeBuddy, your new password is: " + password + " login at " + config.hostname + "/login",
+		from: "CodeBuddyWeb <codebuddyweb@gmail.com>",
+		to: name + " <" + email + ">",
+		subject: "CodeBuddy password"
 	}, function(err, message) {
 		if (err) console.log("ERROR", "sending email:", err);
 		else console.log("INFO", "successfully sent email:", message);
