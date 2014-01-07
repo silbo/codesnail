c3cc548c58728eb5337e046d6d23c2dd9cf42bcd
silps <silver.kuusik@gmail.com>
2014-01-07 07:58:56 +0100

    verfication still not fixed :(

--- config/authentication.js
+++ config/authentication.js
@@ -49,9 +49,8 @@ exports.generateGuest = function generateGuest() {
 /* Simple route middleware to ensure user is authenticated. Otherwise send to login page. */
 exports.ensureAuthenticated = function ensureAuthenticated(req, res, next) {
     console.log("INFO", "login user:", req.user);
-    if (req.isAuthenticated() && req.user.verification) {
-        if (!req.user.verification.verified) req.flash('error', "Please verify your user");
-        else return next();
+    if (req.isAuthenticated()) {
+        return next();
     }
     res.redirect('/login');
 }
