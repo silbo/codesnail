6b614942433bb96cd935d8798e5c8be531ecddc8
silps <silver.kuusik@gmail.com>
2014-01-09 00:07:51 +0100

    refactoring

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -3,14 +3,14 @@ var stream = undefined;
 
 /* When DOM has been loaded */
 window.onload = function() {
-	socket = io.connect('http://www.codesnail.com/profile');
+	socket = io.connect();
 	stream = ss.createStream();
 
-	var box = $('#box');
-	box.on('dragenter', doNothing);
-	box.on('dragover', doNothing);
-	box.text('Drag mugshot here');
-	box.on('drop', function(e) {
+	var box = $("#box");
+	box.on("dragenter", doNothing);
+	box.on("dragover", doNothing);
+	box.text("Drag mugshot here");
+	box.on("drop", function(e) {
 		e.originalEvent.preventDefault();
 		var file = e.originalEvent.dataTransfer.files[0];
 		/* Max mugshot size 100KB */
@@ -19,15 +19,15 @@ window.onload = function() {
 			return;
 		}
 		/* Append the file path the the form */
-		$('form#profile').append('<input type="hidden" name="mugshot" value="'+file.name+'" />');
+		$("form#profile").append('<input type="hidden" name="mugshot" value="'+file.name+'" />');
 
 		/* Upload a file to the server */
-		ss(socket).emit('mugshot', stream, { size: file.size, name: file.name });
+		ss(socket).emit("mugshot", stream, { size: file.size, name: file.name });
 		ss.createBlobReadStream(file).pipe(stream);
 
 		/* Show progress */
-		ss(socket).on('data', function(message) {
-			$('.messages').text(message);
+		ss(socket).on("data", function(message) {
+			$(".messages").text(message);
 		});
 	}); 
 };
