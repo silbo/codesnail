88934491325d82ca133a13ba79686495011ac647
silps <silver.kuusik@gmail.com>
2014-01-09 00:08:02 +0100

    refactoring

--- public/javascripts/study.js
+++ public/javascripts/study.js
@@ -2,7 +2,7 @@
 window.onload = function() {
 	/* ACE editor initialization */
 	$("[id$=code]").each(function() {
-		editor = ace.edit($(this).attr('id'));
+		editor = ace.edit($(this).attr("id"));
 		editor.session.setMode("ace/mode/html");
 		editor.setOptions({ maxLines: 10, minLines: 1 });
 	});
