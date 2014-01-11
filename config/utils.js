9e3d9f17be1cf518fc212c9ec9e7e28f65e31b31
silps <silver.kuusik@gmail.com>
2014-01-11 01:59:16 +0100

    moved auth code

---
+++ config/utils.js
@@ -0,0 +1,64 @@
+/* Add modules */
+var crypto = require('crypto'),
+    db = require('./database'),
+    config = require('./config');
+
+var tasks = [];
+var currentTask = 0;
+
+/* Get new task */
+exports.getTask = function getTask() {
+    /* Update tasks */
+    db.Task.find(function(err, db_tasks) {
+        if (err) console.log("ERROR", "fetching all tasks:", err);
+        else tasks = db_tasks;
+    });
+    if (tasks.length == 0) return { name: "Currently no tasks available" };
+    return { name: "Task " + (currentTask + 1) + ": " + tasks[currentTask].name, points: tasks[currentTask].points };
+}
+
+/* Check if task is complete */
+exports.taskComplete = function taskComplete(code) {
+    if (tasks.length == 0) return false;
+    /* When task was completed */
+    if (code.replace(/\s+/g, '').match(tasks[currentTask].verification)) {
+        currentTask = (currentTask + 1) % tasks.length;
+        return true;
+    }
+    return false;
+}
+
+/* Hashing function */
+exports.calculateHash = function calculateHash(type, text) {
+    var shasum = crypto.createHash(type);
+    shasum.update(text);
+    return shasum.digest("hex");
+}
+
+/* Random function */
+exports.generateRandom = function generateRandom() {
+    /* TODO: Add existing check, or add date + time */
+    var current_date = (new Date()).valueOf().toString();
+    var random = parseInt(Math.random()*100);
+    return random;
+}
+
+/* Generate a random guest account */
+exports.generateGuest = function generateGuest() {
+    var random = exports.generateRandom();
+    var randomEmailHash = exports.calculateHash('md5', random + "@email.com");
+    var user = {
+        name: "Guest" + random,
+        email: random + "@email.com",
+        guest: true,
+        profile: {
+            mugshot: config.gravatar.mugshot + randomEmailHash + "?d=identicon",
+            website: config.gravatar.profile,
+            description: "",
+            points: 0
+        },
+        verification: { verified: true }
+    };
+    console.log("INFO", "generated guest user:", user);
+    return user;
+}
