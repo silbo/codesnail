92f8bbe7a42ebb5b4c001fd29eed5d349b20d6c1
silps <silver.kuusik@gmail.com>
2014-01-11 01:58:13 +0100

    auth to utils

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 /* Add modules */
-var config = require("./config"),
-	auth = require("./authentication"),
-	email = require("./email");
+var utils = require('./utils'),
+	email = require('./email'),
+	config = require('./config');
 
 /* Connect to database */
 var mongoose = require("mongoose");
@@ -69,22 +69,22 @@ UserSchema.pre('save', function(next) {
 	/* When the user logs in with a provider first time */
 	if (this.password == "default") {
 		/* Calculate the password hash */
-		this.password = auth.calculateHash("sha256", this.email + this.joined_date);
+		this.password = utils.calculateHash("sha256", this.email + this.joined_date);
 		/* Set user as verified */
 		this.verification.verified = true;
-		this.verification.verification_hash = auth.calculateHash("sha256", this.email + this.joined_date);
+		this.verification.verification_hash = utils.calculateHash("sha256", this.email + this.joined_date);
 	}
 	/* When the user registers first time */
 	else if (this.verification.verification_hash == "default") {
 		/* Calculate the password hash */
-		this.password = auth.calculateHash("sha256", this.password + this.joined_date);
+		this.password = utils.calculateHash("sha256", this.password + this.joined_date);
 		/* Calculate the verification hash */
-		this.verification.verification_hash = auth.calculateHash("sha256", this.email + this.joined_date);
+		this.verification.verification_hash = utils.calculateHash("sha256", this.email + this.joined_date);
 		/* Send the user the verification email */
 		email.sendRegistrationEmail(this.name, this.email, this.verification.verification_hash);
 	}
 	/* Set the mugshot and website from gravatar */
-	this.profile.mugshot = this.profile.mugshot || config.gravatar.mugshot + auth.calculateHash("md5", this.email) + "?d=identicon";
+	this.profile.mugshot = this.profile.mugshot || config.gravatar.mugshot + utils.calculateHash("md5", this.email) + "?d=identicon";
 	this.profile.website = this.profile.website || "#"
 	next();
 });
