fd91397d3775059cd8d59d5873015f23957da151
silps <silver.kuusik@gmail.com>
2014-01-16 02:31:11 +0100

    divided onlineUsers into different rooms

--- config/socket.js
+++ config/socket.js
@@ -25,67 +25,84 @@ io.set('authorization', passportSocketIo.authorize({
 }));
 
 /* Online users */
-var onlineUsers = {};
+var onlineUsers = { 'study': {}, 'coding': {}, 'sumorobot': {} };
 /* User initiated socket connection */
 io.sockets.on('connection', function(socket) {
-	/* Add user to online users */
+	
+	/* User connected to socketio */
 	console.log("INFO", "socket connection established");
 	console.log("INFO", "socket user:", socket.handshake.user.email);
 	socket.heartbeatTimeout = 5000;
-	onlineUsers[socket.handshake.user.email] = {
-		name: socket.handshake.user.name,
-		email: socket.handshake.user.email,
-		profile: {
-			points: socket.handshake.user.profile.points,
-			mugshot: socket.handshake.user.profile.mugshot,
-			website: socket.handshake.user.profile.website,
-			description: socket.handshake.user.profile.description
-		},
-		code: ""
-	};
-	/* Update the online users for all users */
-	io.sockets.emit("users", onlineUsers);
 
-	/* User asks for someones code */
+	/* Subscribing and unsubscribing to rooms */
+	socket.on('subscribe', function(room) {
+		socket.join(room);
+		/* Delete user from from every room, dont know where he was :P */
+		delete onlineUsers['study'][socket.handshake.user.email];
+		delete onlineUsers['coding'][socket.handshake.user.email];
+		delete onlineUsers['sumorobot'][socket.handshake.user.email];
+		/* Add the user to the room he/she is now */
+		onlineUsers[room][socket.handshake.user.email] = {
+			name: socket.handshake.user.name,
+			profile: {
+				points: socket.handshake.user.profile.points,
+				mugshot: socket.handshake.user.profile.mugshot,
+				description: socket.handshake.user.profile.description
+			}
+		};
+		/* Update the online users for all users in every room */
+		io.sockets.to('study').emit('users', onlineUsers['study']);
+		io.sockets.to('coding').emit('users', onlineUsers['coding']);
+		io.sockets.to('sumorobot').emit('users', onlineUsers['sumorobot']);
+
+		console.log("INFO", "online users:", onlineUsers);
+	});
+
+	/* User sends ping */
 	socket.on('ping', function() {
 		console.log("INFO", "ping received from user:", socket.handshake.user.email);
 	});
 
 	/* User asks for someones code */
-	socket.on('get-code', function(userEmail) {
+	socket.on('get-code', function(room, userEmail) {
 		console.log("INFO", "get user code:", userEmail);
-		socket.emit("receive-code", onlineUsers[userEmail].code);
+		socket.emit('receive-code', onlineUsers[room][userEmail].code);
 	});
 
-	/* User asks for someones code */
-	socket.on('share-code', function(code) {
-		console.log("INFO", "save user code:", socket.handshake.user.email);
-		socket.broadcast.emit("receive-code", code);
+	/* User shares his/her code */
+	socket.on('share-code', function(room, code) {
+		console.log("INFO", "share user code:", socket.handshake.user.email);
+		io.sockets.to(room).broadcast.emit('receive-code', scoket.code);
+	});
+
+	/* User shares his/her chat */
+	socket.on('share-chat', function(room, chat) {
+		console.log("INFO", "user chat:", socket.handshake.user.email);
+		io.sockets.to(room).emit('receive-chat', socket.handshake.user.name, chat);
 	});
 
 	/* User asks for a task */
 	socket.on('get-task', function() {
 		console.log("INFO", "get task:", socket.handshake.user.email);
-		socket.emit("receive-task", utils.getTask());
+		socket.emit('receive-task', utils.getTask());
 	});
 
 	/* User verifies a task */
-	socket.on('verify-task', function(code) {
+	socket.on('verify-task', function(room, code) {
 		console.log("INFO", "verifiying task:", code.replace(/\s+/g, ''));
 		/* Save the users code */
-		onlineUsers[socket.handshake.user.email].code = code;
+		onlineUsers[room][socket.handshake.user.email].code = code;
 		var prev_task = utils.getTask();
 		/* Check if task has been completed */
 		if (utils.taskComplete(code)) {
-			io.sockets.emit("receive-task-verification", socket.handshake.user.name, prev_task.points);
-			io.sockets.emit("receive-task", utils.getTask());
+			/* Send the winner to everyone in the room and update their task */
+			io.sockets.to(room).emit('receive-task-verification', socket.handshake.user.name, prev_task.points);
+			io.sockets.to(room).emit('receive-task', utils.getTask());
 			/* Update user points in his session */
-			onlineUsers[socket.handshake.user.email].profile.points += prev_task.points;
+			onlineUsers[room][socket.handshake.user.email].profile.points += prev_task.points;
 			/* Update the online users for all users */
-			io.sockets.emit("users", onlineUsers);
+			io.sockets.to(room).emit('users', onlineUsers[room]);
 		}
-		else
-			io.sockets.emit("receive-task-verification", "");
 	});
 
 	/* To stream mugshot to the server */
@@ -93,7 +110,7 @@ io.sockets.on('connection', function(socket) {
 		console.log("INFO", "incoming stream size:", meta.size, meta.name)
 		/* Drop the stream if the file is too large max 100KB allowed */
 		if (meta.size > 100000) return;
-		stream.pipe(fs.createWriteStream(__dirname + '/public/images/' + meta.name));
+		stream.pipe(fs.createWriteStream(__dirname + "/public/images/" + meta.name));
 		// Send progress back
 		ss(socket).emit('data', "Mugshot uploaded, click save to update");
 	});
@@ -101,10 +118,14 @@ io.sockets.on('connection', function(socket) {
 	/* User disconnected from socket */
 	socket.on('disconnect', function() {
 		console.log("INFO", "socket user disconnected:", socket.handshake.user.email);
-		/* Delete user from online users */
-		delete onlineUsers[socket.handshake.user.email];
-		/* Update the online users for all users */
-		io.sockets.emit("users", onlineUsers);
+		/* Delete user from from every room, dont know where he is :P */
+		delete onlineUsers['study'][socket.handshake.user.email];
+		delete onlineUsers['coding'][socket.handshake.user.email];
+		delete onlineUsers['sumorobot'][socket.handshake.user.email];
+		/* Update the online users for all users in every room */
+		io.sockets.to('study').emit('users', onlineUsers['study']);
+		io.sockets.to('coding').emit('users', onlineUsers['coding']);
+		io.sockets.to('sumorobot').emit('users', onlineUsers['sumorobot']);
 		/* TODO: When not a guest user, save the points */
 	});
 });
