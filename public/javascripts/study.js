c88d83e4ae23d1ff2bb81e9abf22debcfce464fc
silps <silver.kuusik@gmail.com>
2014-01-19 14:10:46 +0100

    javascripts/study.js removed code collaboration from study page

--- public/javascripts/study.js
+++ public/javascripts/study.js
@@ -1,6 +1,3 @@
-var editor = undefined;
-var oldCode = undefined;
-
 /* When DOM has been loaded */
 window.onload = function() {
 	socket = io.connect();
@@ -23,74 +20,11 @@ window.onload = function() {
 		}
 	});
 
-	/* Receive shared code */
-	socket.on("receive-code", function(code) {
-		editor.setValue(code, 1);
-	});
-
-	/* Receive shared chat */
-	socket.on("receive-chat", function(userName, chat) {
-		$("#chat").append(userName + ": " + chat + "\n");
-		$('#chat').scrollTop($('#chat')[0].scrollHeight);
-	});
-
-	/* Execute when user types */
-	$("#code").keydown(function() {
-		/* Send the code to other users */
-		socket.emit("share-code", "study", editor.getValue());
-	});
-
-	/* Execute when user presses enter */
-	$("#message").keypress(function(e) {
-		if(e.which == 13) {
-			/* Send the chat to other users */
-			socket.emit("share-chat", "study", $("#message").val());
-			/* Clear the message field and blur the submit button */
-			$("#submit-chat").blur();
-			$("#message").val("");
-		}
-	});
-
-	/* Execute when user clicks send */
-	$("#submit-chat").click(function() {
-		/* Send the chat to other users */
-		socket.emit("share-chat", "study", $("#message").val());
-		/* Clear the message field and blur the submit button */
-		$("#submit-chat").blur();
-		$("#message").val("");
-	});
-
 	/* Execute in intervals */
 	setInterval(function() {
 		/* Send ping to keep connection alive */
 		socket.emit("ping");
 	}, 10000);
-
-	/* ACE editor initialization */
-	editor = ace.edit("code");
-	editor.session.setMode("ace/mode/html");
-	editor.setOptions({ maxLines: 10, minLines: 10 });
-	editor.setAutoScrollEditorIntoView(true);
-
-	/* Load emmet for fast coding */
-	ace.config.loadModule("ace/ext/emmet", function() {
-		ace.require("ace/lib/net").loadScript("http://nightwing.github.io/emmet-core/emmet.js", function() {
-			editor.setOption("enableEmmet", true);
-		});
-
-		editor.setOptions({
-			enableSnippets: true,
-			enableBasicAutocompletion: true
-		});
-	});
-
-	/* Enable autocomplete */
-	ace.config.loadModule("ace/ext/language_tools", function() {
-		editor.setOptions({
-			enableSnippets: true,
-			enableBasicAutocompletion: true
-		});
-	});
 
 	/* ACE editor initialization */
 	$("[id$=example]").each(function() {
