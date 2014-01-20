0b025d45512c80f95e8416dd20c77ef6047818a6
silps <silver.kuusik@gmail.com>
2014-01-20 03:38:03 +0100

    config/config.js updated config

--- config/config.js
+++ config/config.js
@@ -29,8 +29,8 @@ if (typeof process.env.OPENSHIFT_APP_NAME !== "undefined") {
 }
 
 /* Email SMTP */
-config.smtp.username = process.env.SMTP_USERNAME;
-config.smtp.password = process.env.SMTP_PASSWORD;
+config.smtp.username = process.env.SMTP_USERNAME || "anonymous";
+config.smtp.password = process.env.SMTP_PASSWORD || "anonymous";
 
 /* Gravatar */
 config.gravatar.mugshot = "http://www.gravatar.com/avatar/";
@@ -41,42 +41,42 @@ config.google.gdata_scopes = [
         "https://www.googleapis.com/auth/userinfo.email",
         "https://www.googleapis.com/auth/userinfo.profile"
 ];
-config.google.consumer_key = process.env.GOOGLE_KEY;;
-config.google.consumer_secret= process.env.GOOGLE_SECRET;
+config.google.consumer_key = process.env.GOOGLE_KEY || "anonymous";
+config.google.consumer_secret= process.env.GOOGLE_SECRET || "anonymous";
 config.google.auth = "/auth/google";
 config.google.callback = "/auth/google/callback";
 
 /* Twitter OAuth */
-config.twitter.consumer_key = process.env.TWITTER_KEY;
-config.twitter.consumer_secret = process.env.TWITTER_SECRET;
+config.twitter.consumer_key = process.env.TWITTER_KEY || "anonymous";
+config.twitter.consumer_secret = process.env.TWITTER_SECRET || "anonymous";
 config.twitter.auth = "/auth/twitter";
 config.twitter.callback = "/auth/twitter/callback";
 
 /* Facebook OAuth2 */
-config.facebook.consumer_key = process.env.FACEBOOK_KEY;
-config.facebook.consumer_secret = process.env.FACEBOOK_SECRET;
+config.facebook.consumer_key = process.env.FACEBOOK_KEY || "anonymous";
+config.facebook.consumer_secret = process.env.FACEBOOK_SECRET || "anonymous";
 config.facebook.auth = "/auth/facebook";
 config.facebook.callback = "/auth/facebook/callback";
 
 /* Linkedin OAuth */
-config.linkedin.consumer_key = process.env.LINKEDIN_KEY;
-config.linkedin.consumer_secret = process.env.LINKEDIN_SECRET;
+config.linkedin.consumer_key = process.env.LINKEDIN_KEY || "anonymous";
+config.linkedin.consumer_secret = process.env.LINKEDIN_SECRET || "anonymous";
 config.linkedin.auth = "/auth/linkedin";
 config.linkedin.callback = "/auth/linkedin/callback";
 
 /* GitHub OAuth2 */
-config.github.consumer_key = process.env.GITHUB_KEY;
-config.github.consumer_secret = process.env.GITHUB_SECRET;
+config.github.consumer_key = process.env.GITHUB_KEY || "anonymous";
+config.github.consumer_secret = process.env.GITHUB_SECRET || "anonymous";
 config.github.auth = "/auth/github";
 config.github.callback = "/auth/github/callback";
 
 /* OAuth logins */
 config.logins = [
-        ["Google", config.google.auth],
-        ["Twitter", config.twitter.auth],
-        ["Facebook", config.facebook.auth],
-        ["LinkedIn", config.linkedin.auth],
-        ["GitHub", config.github.auth]
+	["Google", config.google.auth],
+	["Twitter", config.twitter.auth],
+	["Facebook", config.facebook.auth],
+	["LinkedIn", config.linkedin.auth],
+	["GitHub", config.github.auth]
 ];
 
 module.exports = config;
