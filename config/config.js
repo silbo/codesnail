99b64691f113b5c51a88274f6603b6e370e143e5
silps <silver.kuusik@gmail.com>
2014-02-28 20:02:40 +0100

    config/config.js updated configuration

--- config/config.js
+++ config/config.js
@@ -1,37 +1,29 @@
-var config = {
-	smtp: {},
-	gravatar: {},
-	google: {},
-	twitter: {},
-	facebook: {},
-	linkedin: {},
-	github: {}
-};
+var config = { smtp: {}, gravatar: {}, google: {}, twitter: {}, facebook: {}, linkedin: {},	github: {} };
 
 /* App hostname and port and session secret */
-config.session_secret = process.env.SESSION_SECRET || "super-secret-u-will-never-guess";
-config.host = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "localhost";
+config.app_name = process.env.APP_NAME || "default";
+config.app_email = process.env.APP_EMAIL || "default";
+config.admin_name = process.env.ADMIN_NAME || "default";
+config.admin_email = process.env.ADMIN_EMAIL || "default";
+config.hostname = process.env.HOSTNAME || "http://localhost:3000";
 config.port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
+config.host = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "localhost";
+config.session_secret = process.env.SESSION_SECRET || "default";
+config.database_url = process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || "mongodb://localhsot:27017/codesnail";
 
-/* Database */
-/* For Openshift */
+/* Database For Openshift */
 if (typeof process.env.OPENSHIFT_APP_NAME !== "undefined") {
 	config.database_url = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
 	process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
 	process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
 	process.env.OPENSHIFT_MONGODB_DB_PORT + "/" +
 	process.env.OPENSHIFT_APP_NAME;
-	config.hostname = "https://codebuddy-students.rhcloud.com";
-/* For others */
-} else {
-	config.database_url = process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || "mongodb://codesnail:*C0d3Sn41l*@ds061278.mongolab.com:61278/codesnail";
-	//"mongodb://localhost:27017/codesnail";
-	config.hostname = "http://www.codesnail.com";
 }
 
 /* Email SMTP */
-config.smtp.username = "codebuddyweb";
-config.smtp.password = "StudentsofCodebuddy123";
+config.smtp.host = process.env.SMTP_HOST || "default";
+config.smtp.username = process.env.SMTP_USERNAME || "default";
+config.smtp.password = process.env.SMTP_PASSWORD || "default";
 
 /* Gravatar */
 config.gravatar.mugshot = "http://www.gravatar.com/avatar/";
@@ -42,32 +34,32 @@ config.google.gdata_scopes = [
 	"https://www.googleapis.com/auth/userinfo.email",
 	"https://www.googleapis.com/auth/userinfo.profile"
 ];
-config.google.consumer_key = "567843820810-o8k72j921k7eiuq4mr9ikavabg9k80qf.apps.googleusercontent.com";
-config.google.consumer_secret= "0XlNjSGiTo4qx-CKx02SAVFS";
+config.google.consumer_key = process.env.GOOGLE_KEY || "default";
+config.google.consumer_secret= process.env.GOOGLE_SECRET || "default";
 config.google.auth = "/auth/google";
 config.google.callback = "/auth/google/callback";
 
 /* Twitter OAuth */
-config.twitter.consumer_key = "IoVfcWOfyyomVMjBxRxAQ";
-config.twitter.consumer_secret = "ik74sO5Z2gKwEM0wANqBza5bIx6jf6XBnp3fQ90td2M";
+config.twitter.consumer_key = process.env.TWITTER_KEY || "default";
+config.twitter.consumer_secret = process.env.TWITTER_SECRET || "default";
 config.twitter.auth = "/auth/twitter";
 config.twitter.callback = "/auth/twitter/callback";
 
 /* Facebook OAuth2 */
-config.facebook.consumer_key = "630550343658490";
-config.facebook.consumer_secret = "d3fd0d2c1b2407694b3452e54bc93d25";
+config.facebook.consumer_key = process.env.FACEBOOK_KEY || "default";
+config.facebook.consumer_secret = process.env.FACEBOOK_SECRET || "default";
 config.facebook.auth = "/auth/facebook";
 config.facebook.callback = "/auth/facebook/callback";
 
 /* Linkedin OAuth */
-config.linkedin.consumer_key = "77x0wged6rzc37";
-config.linkedin.consumer_secret = "nFSpFF5dfM1H51uH";
+config.linkedin.consumer_key = process.env.LINKEDIN_KEY || "default";
+config.linkedin.consumer_secret = process.env.LINKEDIN_SECRET || "default";
 config.linkedin.auth = "/auth/linkedin";
 config.linkedin.callback = "/auth/linkedin/callback";
 
 /* GitHub OAuth2 */
-config.github.consumer_key = "d4f83e69327ff78683bc";
-config.github.consumer_secret = "afd495b77ac3490d2fe914532119ba3262d7ae66";
+config.github.consumer_key = process.env.GITHUB_KEY || "default";
+config.github.consumer_secret = process.env.GITHUB_SECRET || "default";
 config.github.auth = "/auth/github";
 config.github.callback = "/auth/github/callback";
 
