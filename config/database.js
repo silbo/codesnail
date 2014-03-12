4ee810672bcc6dc18a38ec94b8678e8befff04db
silps <silver.kuusik@gmail.com>
2014-03-12 07:23:00 +0100

    config/database.js updated user object

--- config/database.js
+++ config/database.js
@@ -51,25 +51,25 @@ var TrackSchema = new Schema({
 });
 
 var UserSchema = new Schema({
-	username: { type: String, unique: true, required: true, validate: /^[a-z0-9_-]{4,15}$/ },
 	name: { type: String, required: true },
-	email: { type: String, unique: true, lowercase: true, trim: true, required: true, validate: /\b[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\b/ },
 	password: { type: String, default: "" },
+	username: { type: String, unique: true, required: true, validate: /^[a-z0-9_-]{4,15}$/ },
+	email: { type: String, unique: true, lowercase: true, trim: true, required: true, validate: /\b[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\b/ },
 	verification: {
-		verified: { type: Boolean, required: true, default: false },
-		verification_hash: { type: String, default: "" }
+		verification_hash: { type: String },
+		verified: { type: Boolean, required: true, default: false }
 	},
 	profile: {
-		joined_date: { type: Date, required: true, default: Date.now },
 		points: { type: Number, default: 0 },
 		mugshot: { type: String, default: "" },
 		website: { type: String, default: "" },
 		location: { type: String, default: "" },
 		description: { type: String, default: "" },
 		badges: [{ type: ObjectId, ref: 'Badge' }],
+		tracks: [{ type: ObjectId, ref: 'Track' }],
+		joined_date: { type: Date, required: true },
 		providers: [{ type: ObjectId, ref: 'Provider' }],
 	},
-	tracks: [{ type: ObjectId, ref: 'Track' }]
 });
 
 /* Database objects */
